c6105fb6b1d65d85732e6b3000cb219771ff5b5e
zhaotianff <zhaotianff@163.com>
2021-05-25 17:25:06 +0800

    Network util code

---
+++ ZTI.Tools/ZTI.Tools/Network.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Collections;
+using System.Linq;
+using System.Net;
+
+namespace ZTI.Tools
+{
+    public class Network
+    {
+        public static IPAddress GetCurrentInterNetworkIP()
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (var item in interfaces)
+            {
+                if (item.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    IPInterfaceProperties property = item.GetIPProperties();
+                    foreach (UnicastIPAddressInformation ipAddress in property.UnicastAddresses)
+                    {
+                        if (ipAddress.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            return ipAddress.Address;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
