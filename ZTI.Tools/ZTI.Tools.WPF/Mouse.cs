516c4424f8f2c5594f89a0f8bda5a0470a267df9
zhaotianff <zhaotianff@163.com>
2021-07-31 21:23:43 +0800

    mouse code

---
+++ ZTI.Tools/ZTI.Tools.WPF/Mouse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZTI.Tools.WPF.Definitions;
+
+namespace ZTI.Tools.WPF
+{
+    public static class Mouse
+    {
+        public static void OnMouseClick<T>(Action<T> action) where T : class,new()
+        {
+
+        }
+
+        public static void SetCursor(string cursorFile)
+        {
+            if (System.IO.File.Exists(cursorFile) == false)
+                return;
+
+            foreach(System.Windows.Window window in System.Windows.Application.Current.Windows)
+            {
+                window.Cursor = new System.Windows.Input.Cursor(cursorFile);
+            }
+        }
+
+        public static void SetSystemCursor(OCR_TYPE oCR_TYPE, string cursorFile)
+        {
+
+        }
+    }
+}
