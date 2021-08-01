a294fbec7a199f2259eddccb4f53e8c1935d4b51
zhaotianff <zhaotianff@163.com>
2021-08-01 16:38:10 +0800

    file

---
+++ ZTI.Tools/ZTI.Tools/File.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTI.Tools
+{
+    public class File
+    {
+        public void Rename(string filePath,string name)
+        {
+            var newFilePath = System.Text.RegularExpressions.Regex.Replace()
+            System.IO.File.Move(filePath, System.Text.RegularExpressions.Regex.Replace(System.IO.Path.GetFileName(filePath),""));
+        }
+    }
+}
